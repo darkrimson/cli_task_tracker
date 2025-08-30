eba02efc229867d993dabf5a28fdfb9d87976ea3
darkrimson <arsanov2003@yandex.ru>
2025-08-30 19:24:45 +0300

    add storage methods

--- storage.go
+++ storage.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func LoadTask(filename string) ([]Task, error) {
+	var tasks []Task
+
+	file, err := os.Open(filename)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return tasks, nil
+		}
+		return nil, err
+	}
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(&tasks)
+	if err != nil {
+		fmt.Println("Ошибка при преобразовании JSON: ", err)
+	}
+	return tasks, err
+}
+
+func SaveTask(filename string, tasks []Task) {
+	file, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Ошибка при сохрании файла: ", err)
+	}
+	defer file.Close()
+
+	err = json.NewEncoder(file).Encode(tasks)
+	if err != nil {
+		fmt.Println("Ошибка при преобразовании в JSON ", err)
+	}
+}
